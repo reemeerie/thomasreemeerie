e1a8eef13d0c1dbcdede8a73a38622d635e22ebe
thomasReemeerie <thomasreemeerie0@gmail.com>
2023-03-21 00:42:42 -0300

    ayuda2

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -7,11 +7,12 @@ const Banner = () => {
     <>
     <div id='home'></div>
       <div className="contPresentation">
+      <div className='blackScreen'></div>
             <div className="presentation">
                 <h1 className='desc'>Thomas Reemeerie</h1>
                 <p className="desc2" >Web developer</p>
             </div>
-            <div className='blackScreen'></div>
+            
       </div>
     </>
   )
